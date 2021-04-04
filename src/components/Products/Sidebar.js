7b57718e8f3ac582035e273e32edb9c997a603a0
joyan11 <joyan.s03@gmail.com>
2021-04-04 18:49:45 +0530

    refactor: created new component for sidebar filters

---
+++ src/components/Products/Sidebar.js
@@ -0,0 +1,84 @@
+import { useMainContext } from "../../context/context";
+
+export const Sidebar = () => {
+  const {
+    stockSort,
+    fastDelivery,
+    priceRange,
+    teamFilter,
+    sortByStock,
+    sortByDelivery,
+    sortBySliderRange,
+    sortByTeam,
+    clearAllFilters,
+  } = useMainContext();
+  return (
+    <>
+      <div className="sidebar">
+        <div className="filter">
+          <div className="team-filter">
+            <label htmlFor="teamfilter">Sort by Teams:</label>
+            <select
+              className="teamselection"
+              name="teamfilter"
+              id="teamfilter"
+              value={teamFilter}
+              onChange={(e) => sortByTeam(e)}>
+              <option value="allteams">All</option>
+              <option value="manutd">Manchester United</option>
+              <option value="mancity">Manchester City</option>
+              <option value="barcelona">Barcelona</option>
+              <option value="juventus">Juventus</option>
+              <option value="intermilan">Inter Milan</option>
+              <option value="acmilan">AC Milan</option>
+              <option value="arsenal">Arsenal</option>
+              <option value="chelsea">Chelsea</option>
+              <option value="dortmund">Dortmund</option>
+              <option value="liverpool">Liverpool</option>
+            </select>
+          </div>
+          <div className="stock-filter">
+            <input
+              type="checkbox"
+              name="stockfilter"
+              id="stockfilter"
+              checked={stockSort === "totalstock" ? false : true}
+              onChange={(e) => sortByStock(e)}
+            />
+            <label htmlFor="stockfilter">Only In Stock</label>
+          </div>
+          <div className="delivery-filter">
+            <input
+              type="checkbox"
+              name=""
+              id="stockfilter"
+              checked={fastDelivery === "allDeliveries" ? false : true}
+              onChange={(e) => sortByDelivery(e)}
+            />
+            <label htmlFor="stockfilter">Fast Delivery</label>
+          </div>
+          <div className="price-slider">
+            <p>Price range:</p>
+            <input
+              min="799"
+              max="2099"
+              type="range"
+              name="slider"
+              id="slider"
+              value={priceRange}
+              onChange={(e) => sortBySliderRange(e)}
+            />
+            <p> &#8377; {priceRange}</p>
+          </div>
+          <div className="clearFilter">
+            <button
+              className="btn btn-secondary"
+              onClick={() => clearAllFilters()}>
+              Clear Filters
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
