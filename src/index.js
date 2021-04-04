fdbf3b8a3d29465704bb8653110bf074e1421828
joyan11 <joyan.s03@gmail.com>
2021-04-04 18:47:07 +0530

    chore: removed unnecessary code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { MainProvider } from "./context/context";
+// import setupMockServer from "./api/mock.server";
+
+// setupMockServer();
 
 ReactDOM.render(
   <React.StrictMode>
