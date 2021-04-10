efcb8c0d7b075beec450542be6f310ee4290f226
joyan11 <joyan.s03@gmail.com>
2021-04-10 10:50:58 +0530

    refactor: added empty cart message

---
+++ src/components/Cart/CartEmpty.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export function CartEmpty() {
+  return (
+    <div className="wishlist-empty-container">
+      <p className="text text-xl text--center empty-text">Your Cart Is Empty</p>
+      <Link to="/products" className="router-link">
+        <button className="btn btn--round btn-secondary">Shop Now</button>
+      </Link>
+    </div>
+  );
+}
