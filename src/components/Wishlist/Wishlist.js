dc7e76d738217f746787fd669985443a1e39f48c
joyan11 <joyan.s03@gmail.com>
2021-04-10 10:53:13 +0530

    refactor: added wishlist empty message

--- src/components/Wishlist/Wishlist.js
+++ src/components/Wishlist/Wishlist.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { useMainContext } from "../../context/context";
 import { Wishcard } from "./Wishcard";
+import { Wishlistempty } from "./Wishlistempty";
 import "../../css/wishlist.css";
 
 export const Wishlist = () => {
-  const { dispatch } = useMainContext();
-  return (
-    <div className="wishlist-section">
-      <div className="wish-container">
-        <Wishcard />
+  const { wishList, dispatch } = useMainContext();
+
+  if (wishList.length === 0) {
+    return (
+      <div className="wishlist-section empty">
+        <Wishlistempty />
       </div>
-      <button
-        className="btn btn-secondary"
-        onClick={() => dispatch({ type: "CLEAR_WISHLIST" })}>
-        Clear Wishlist
-      </button>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="wishlist-section">
+        <div className="wish-container">
+          <Wishcard />
+        </div>
+        <button
+          className="btn btn-secondary"
+          onClick={() => dispatch({ type: "CLEAR_WISHLIST" })}>
+          Clear Wishlist
+        </button>
+      </div>
+    );
+  }
 };
