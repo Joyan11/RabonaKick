90f94e016fc168719306391839021884e0a1455a
joyan11 <joyan.s03@gmail.com>
2021-04-10 10:53:43 +0530

    refactor: created wishlist empty component

---
+++ src/components/Wishlist/Wishlistempty.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export function Wishlistempty() {
+  return (
+    <div className="wishlist-empty-container">
+      <p className="text text-xl text--center empty-text">
+        It's Free To Make a Wish
+      </p>
+      <Link to="/products" className="router-link">
+        <button className="btn btn--round btn-secondary">
+          Continue Browsing
+        </button>
+      </Link>
+    </div>
+  );
+}
