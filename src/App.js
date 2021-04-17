51dc1604c2dfe7c8475359f8afe507cefdf9a3f5
joyan11 <joyan.s03@gmail.com>
2021-04-17 10:09:26 +0530

    refactor: shifted navbar in main

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Wishlist } from "./components/Wishlist/Wishlist";
 import { Home } from "./components/Home";
 import { Modal } from "./components/Modal";
 import { Navbar } from "./components/Navbar";
-
+import { Footer } from "./components/Footer";
 export default function App() {
   const {
     totalCartQuantity,
@@ -18,15 +18,20 @@ export default function App() {
 
   return (
     <div className="App">
-      <Navbar totalCartQuantity={totalCartQuantity} totalWishes={totalWishes} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/wishList" element={<Wishlist />} />
-      </Routes>
-
-      {displayModal && <Modal modalContent={modalContent} />}
+      <main>
+        <Navbar
+          totalCartQuantity={totalCartQuantity}
+          totalWishes={totalWishes}
+        />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/wishList" element={<Wishlist />} />
+        </Routes>
+        {displayModal && <Modal modalContent={modalContent} />}
+        <Footer />
+      </main>
     </div>
   );
 }
