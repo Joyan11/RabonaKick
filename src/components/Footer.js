04deec0d943006e72ce583804329fb6a7dd7e2fc
joyan11 <joyan.s03@gmail.com>
2021-04-17 10:22:20 +0530

    refactor: added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <p>&#169; 2021 RabonaKick All rights reserved. Made by Joyan Serrao</p>
+    </div>
+  );
+};
