2e99a01624189bea59bb325d96788adf7ad8c307
joyan11 <joyan.s03@gmail.com>
2021-04-27 15:20:31 +0530

    refactor: changed loader name

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
-export const SpinLoader = () => {
+export const PuffLoader = () => {
   return (
     <Loader
       type="Puff"
