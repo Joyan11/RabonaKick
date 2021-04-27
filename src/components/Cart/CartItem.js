a53ba3b6fecc08c6ef44325f4d30d1539b81201c
joyan11 <joyan.s03@gmail.com>
2021-04-27 15:20:57 +0530

    feat: integrated backend for cart items

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,34 +1,107 @@
 import { useMainContext } from "../../context/context";
-
+import axios from "axios";
 export const CartItem = () => {
-  const { cart, dispatch } = useMainContext();
+  const { cart, cartId, dispatch } = useMainContext();
+
+  const setQuantity = async (cartId, productId, quantity, operation) => {
+    if (operation === "inc") {
+      try {
+        const {
+          status,
+          data: {
+            cartItems: { _id: cartid, products },
+          },
+        } = await axios.post(
+          `https://rabonaserver.joyan11.repl.co/cart/${cartId}/${productId}`,
+          {
+            type: operation,
+          }
+        );
+
+        if (status === 201) {
+          dispatch({ type: "INCREASE_QUANTITY", payload: productId });
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    } else if (operation === "dec" && quantity > 1) {
+      try {
+        const {
+          status,
+          data: {
+            cartItems: { _id: cartid, products },
+          },
+        } = await axios.post(
+          `https://rabonaserver.joyan11.repl.co/cart/${cartId}/${productId}`,
+          {
+            type: operation,
+          }
+        );
+        if (status === 201) {
+          dispatch({ type: "DECREASE_QUANTITY", payload: productId });
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  };
+
+  const removeFromCart = async (cartId, productid) => {
+    try {
+      const {
+        status,
+        data: {
+          cartItems: { _id: cartid, products },
+        },
+      } = await axios.delete(
+        `https://rabonaserver.joyan11.repl.co/cart/${cartId}/${productid}`
+      );
+      if (status === 200) {
+        dispatch({ type: "REMOVE_ITEM", payload: productid });
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <>
-      {cart.map((item) => {
+      {cart.map(({ productId: product, quantity }) => {
         return (
           <div
-            key={item._id}
+            key={product._id}
             className="card card--horizontal border cart--card">
             <figure className="card--image">
-              <img src={item.image} alt="{item.name}" />
+              <img src={product.image} alt="{item.name}" />
             </figure>
             <div className="card--body cart--card-body">
-              <span className="card--title">{item.name}</span>
-              <p className="card--text"> &#8377;{item.price * item.quantity}</p>
+              <span className="card--title">{product.name}</span>
+              <p className="card--text"> &#8377;{product.price * quantity}</p>
               <div className="cart--buttons-container">
                 <button
                   className="cart--buttons btn btn-primary"
+                  // onClick={() =>
+                  //   dispatch({
+                  //     type: "DECREASE_QUANTITY",
+                  //     payload: product._id,
+                  //   })
+                  // }
                   onClick={() =>
-                    dispatch({ type: "DECREASE_QUANTITY", payload: item._id })
+                    setQuantity(cartId, product._id, quantity, "dec")
                   }>
                   -
                 </button>
-                <p>{item.quantity}</p>
+                <p>{quantity}</p>
                 <button
                   className="cart--buttons btn btn-primary"
+                  // onClick={() =>
+                  //   dispatch({
+                  //     type: "INCREASE_QUANTITY",
+                  //     payload: product._id,
+                  //   })
+                  // }
                   onClick={() =>
-                    dispatch({ type: "INCREASE_QUANTITY", payload: item._id })
+                    setQuantity(cartId, product._id, quantity, "inc")
                   }>
                   +
                 </button>
@@ -37,9 +110,10 @@ export const CartItem = () => {
             <ion-icon
               class="cart-dismiss"
               name="close-circle-outline"
-              onClick={() =>
-                dispatch({ type: "REMOVE_ITEM", payload: item._id })
-              }></ion-icon>
+              // onClick={() =>
+              //   dispatch({ type: "REMOVE_ITEM", payload: product._id })
+              // }
+              onClick={() => removeFromCart(cartId, product._id)}></ion-icon>
           </div>
         );
       })}
