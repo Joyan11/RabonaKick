d2f85b799aed18f6548940587276643633028f0f
joyan11 <joyan.s03@gmail.com>
2021-07-08 21:08:51 +0530

    refactor: minor change with token

--- src/utils/cartAuthCheck.js
+++ src/utils/cartAuthCheck.js
@@ -2,7 +2,7 @@ import { addToCart } from "../api/cart/addToCart";
 
 export const cartAuthCheck = (token, navigate, itemid, dispatch) => {
   if (token) {
-    addToCart(itemid, dispatch, token);
+    addToCart(itemid, dispatch);
   } else {
     navigate("/login");
   }
