213367efb2c1925fc1cdd10f6e48050f7b9c5c60
joyan11 <joyan.s03@gmail.com>
2021-07-08 21:03:38 +0530

    chore: deleted file

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = () => {
-  return (
-    <div className="footer">
-      <p>&#169; 2021 RabonaKick All rights reserved. Made by Joyan Serrao</p>
-    </div>
-  );
-};
